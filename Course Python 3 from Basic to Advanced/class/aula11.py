4f66ae1f957ffe99b94785ea13b896d600e574b6
Bruno Alves Comitre <bruno-comitre@hotmail.com>
2020-07-06 20:33:45 -0300

    Adicionado aulas

---
+++ Course Python 3 from Basic to Advanced/class/aula11.py
@@ -0,0 +1,16 @@
+# String: nome
+nome = 'Bruno'
+print(nome, type(nome))
+
+# Int: idade
+idade = 26
+print(idade, type(idade))
+
+# FLoat: altura
+altura = 1.83
+print(altura, type(altura))
+
+# Boolean: maior : É maior de idade 10 > 20
+maior = 18
+resultado = idade >= idade
+print(resultado, type(resultado))
---
+++ Course Python 3 from Basic to Advanced/class/aula13.py
@@ -0,0 +1,7 @@
+nome = 'Teste'
+numero = 80
+numero_float = 80.0000
+
+print(f'{nome:.2s}')
+print(f'{numero:d}')
+print(f'{numero_float:.2f}')
